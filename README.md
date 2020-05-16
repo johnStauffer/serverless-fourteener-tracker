732427a5c8be1be97ccd447d8067cadcbd128cf5
John Stauffer <john.stauffer.d@gmail.com>
2020-05-16 11:07:15 -0600

    Getting local dynamo working and some CRUD for user table

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 #fourteener-tracker
 
+# Setup
+Set AWS credentials in environment with serverless-dotenv-plugin   
+in .env file   
+```AWS_ACCESS_KEY_ID=<your-key-here>```  
+```AWS_SECRET_ACCESS_KEY=<your-secret-key-here>```
--- services/user-api/handler.js
+++ services/user-api/handler.js
@@ -4,47 +4,84 @@ import dynamodb from 'serverless-dynamodb-client';
 const dynamoClient = dynamodb.doc;
 
 const typeDefs = gql`
-    type Query {
-        hello: String
-        getUser(id: ID!): User
-    }
-
     type Location {
         city: String
         state: String
     }
 
     type User {
-        userId: Int!
+        userId: String!
         firstName: String!
         lastName: String!
         email: String!
         location: Location
     }
+
+    input UserInput {
+        userId: String!
+        firstName: String!
+        lastName: String!
+        email: String!
+        location: LocationInput
+    }
+
+    input LocationInput {
+        city: String
+        state: String
+    }
+
+    type Query {
+        getUser(id: ID!): User
+    }
+
+    type Mutation {
+        createUser(input: UserInput!): String
+    }
 `;
 
-const getUser = (userId) => {
-    const params = {
-        TableName: process.env.USERS_TABLE_NAME,
-        Key: {
-            HashKey: userId
-        }
-    };
-    console.log(params);
-    return dynamoClient.scan(params, (err, data) => {
-        if (err) {
-            throw err;
-        } else return data;
-    });
-};
+const getUser = userId => new Promise((resolve, reject) => {
+    dynamoClient.get(
+        {
+            TableName: process.env.USERS_TABLE_NAME,
+            Key: {userId}
+        },
+        (err, data) => {
+            if (err) reject(err);
+            else resolve(data.Item);
+        });
+});
+
+const putUser = user => new Promise((resolve, reject) => {
+    dynamoClient.put({
+            TableName: process.env.USERS_TABLE_NAME,
+            Item: user
+        },
+        (err, data) => {
+            if (err) reject(err);
+            else resolve();
+        });
+});
 
 const resolvers = {
     Query: {
-        hello: () => 'Hello world!',
         getUser: (parent, {id}, context, info) => {
-            return getUser(id);
-        }
+            return getUser(id)
+                .then(user => {
+                    console.log(`retrieved user: ${id}`);
+                    return user;
+                });
+        },
     },
+    Mutation: {
+        createUser: (parent, {input}, context, info) => {
+            return putUser(input)
+                .then(() => getUser(input.userId))
+                .then(user => {
+                    console.log(`Created user: ${user.userId}`);
+                    return user;
+                });
+        },
+    }
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
