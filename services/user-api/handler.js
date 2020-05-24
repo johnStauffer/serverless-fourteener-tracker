9e806f1fb4b490ab944036c1b1037ed2bd88945e
John Stauffer <john.stauffer.d@gmail.com>
2020-05-24 16:17:00 -0600

    Returning promised from graphql

--- services/user-api/handler.js
+++ services/user-api/handler.js
@@ -1,5 +1,6 @@
-import {ApolloServer, gql} from 'apollo-server-lambda';
+import {ApolloServer, UserInputError, gql} from 'apollo-server-lambda';
 import dynamodb from 'serverless-dynamodb-client';
+import normalizeEmail from "normalize-email";
 
 const dynamoClient = dynamodb.doc;
 
@@ -10,10 +11,10 @@ const typeDefs = gql`
     }
 
     type User {
-        userId: String!
-        firstName: String!
-        lastName: String!
-        email: String!
+        userId: String
+        firstName: String
+        lastName: String
+        email: String
         location: Location
     }
 
@@ -35,51 +36,96 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        createUser(input: UserInput!): String
+        createUser(input: UserInput!): User
     }
 `;
 
-const getUser = userId => new Promise((resolve, reject) => {
-    dynamoClient.get(
+function getUser(userId) {
+    return dynamoClient.get(
         {
             TableName: process.env.USERS_TABLE_NAME,
             Key: {userId}
-        },
-        (err, data) => {
-            if (err) reject(err);
-            else resolve(data.Item);
-        });
-});
+        }, (err, data) => {
+            if (err) console.log(err.message);
+            else return data.Item;
+        }
+    ).promise().then(response => {
+        return response.Item;
+    });
+}
 
-const putUser = user => new Promise((resolve, reject) => {
-    dynamoClient.put({
-            TableName: process.env.USERS_TABLE_NAME,
-            Item: user
-        },
-        (err, data) => {
-            if (err) reject(err);
-            else resolve();
-        });
-});
+function getUserByEmail(email) {
+    return new Promise((resolve, reject) => {
+            dynamoClient.scan({
+                TableName: process.env.USERS_TABLE_NAME,
+                FilterExpression: "#email = :email",
+                ExpressionAttributeNames: {
+                    '#email': 'email'
+                },
+                ExpressionAttributeValues: {
+                    ':email': email
+                },
+                Limit: 1
+            }).promise().then((result, err) => {
+                if (err) reject(err);
+                resolve((result.Count > 0) ? result.Items[0] : null);
+            })
+        }
+    );
+}
+
+function putUser(user) {
+    return dynamoClient.put({
+        TableName: process.env.USERS_TABLE_NAME,
+        ReturnValues: 'ALL_OLD',
+        Item: user
+    }).promise();
+}
+
+async function createUser(userInput) {
+    return new Promise((resolve, reject) => {
+        const normalizedUser = normalizeUser(userInput);
+        getUserByEmail(normalizedUser)
+            .then(existingUser => {
+                if (existingUser != null) {
+                    reject(new Error(`User already exists for email: ${normalizedUser.email}`))
+                } else {
+                    return putUser(normalizedUser);
+                }
+            })
+            .then(() => getUser(userInput.userId).then(user => {
+                resolve(user);
+            }));
+    });
+}
+
+
+const normalizeUser = userInput => {
+    return ({
+        userId: userInput.userId,
+        firstName: userInput.firstName.toLowerCase(),
+        lastName: userInput.lastName.toLowerCase(),
+        email: normalizeEmail(userInput.email),
+        location: userInput.location,
+    });
+};
 
 const resolvers = {
     Query: {
         getUser: (parent, {id}, context, info) => {
             return getUser(id)
                 .then(user => {
-                    console.log(`retrieved user: ${id}`);
                     return user;
                 });
         },
     },
     Mutation: {
         createUser: (parent, {input}, context, info) => {
-            return putUser(input)
-                .then(() => getUser(input.userId))
-                .then(user => {
-                    console.log(`Created user: ${user.userId}`);
-                    return user;
-                });
+            try {
+                return createUser(input)
+            } catch (e) {
+                throw new UserInputError(`User already registered with email: ${input.email}`);
+            }
         },
     }
 };
